42b084980c9678e32a048a1ffea040f67e1075f0
Rafal Kus <rkus8710@gmail.com>
2022-06-28 13:43:39 +0200

    added task model

---
+++ src/app/model/task.ts
@@ -0,0 +1,5 @@
+export interface Task {
+    text: string,
+    date: string,
+    done: boolean
+}
---
+++ src/app/services/task.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  private apiUrl:string = 'http://localhost:3000/tasks'; 
+
+  constructor(private httpClient: HttpClient) { }
+
+  
+}
